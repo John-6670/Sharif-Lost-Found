d1a38ca3a43481c4f4c94c1920d64a32b317b25c
john6670 <john2005mm@gmail.com>
2025-12-28 11:49:19 +0330

    added User Register logic

---
+++ backend/django-api/users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name', 'is_active', 'created_at']
