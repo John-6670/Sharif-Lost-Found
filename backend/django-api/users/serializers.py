6a639312476e37e5f8ac21edcd62e1f2f430a850
john6670 <john2005mm@gmail.com>
2026-01-01 17:39:40 +0330

    added swagger doc

--- backend/django-api/users/serializers.py
+++ backend/django-api/users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import User
 
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
@@ -9,22 +8,19 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'name', 'password']
 
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
-
-
-class UserSerializer(serializers.ModelSerializer):
+class UserPublicSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'created_at', 'verified']
-
 
 class OTPRequestSerializer(serializers.Serializer):
     email = serializers.EmailField()
     name = serializers.CharField(max_length=255)
     password = serializers.CharField(min_length=8)
 
-
 class OTPVerifySerializer(serializers.Serializer):
     email = serializers.EmailField()
     otp = serializers.CharField(max_length=6)
+
+class ResendOTPSerializer(serializers.Serializer):
+    email = serializers.EmailField()
